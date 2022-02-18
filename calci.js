584ba27cc1b2f7fb05db7abea1b09b9d48a951ae
Vaishnavi <vaishusawant2002@gmail.com>
2022-02-18 15:16:50 +0530

    First commit

---
+++ calci.js
@@ -0,0 +1,26 @@
+ var screen=document.querySelector('#screen');
+    var btn=document.querySelectorAll('.btn');
+
+    /*For getting the value of btn*/
+    for(item of btn)
+    {
+        item.addEventListener('click',(e)=>{
+            btntext=e.target.innerText;
+
+            if(btntext =='×')
+            {
+                btntext= '*';
+            }
+
+            if(btntext=='÷')
+            {
+                btntext='/';
+            }
+            screen.value+=btntext;
+        });
+    }
+
+    function backspc()
+    {
+        screen.value=screen.value.substr(0,screen.value.length-1);
+    }
